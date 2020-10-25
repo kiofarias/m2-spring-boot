db113d8cbd960d8500b4e8f325a1f01a6beb9c2b
Francisco farias <kiofarias@gmail.com>
2020-10-25 10:01:19 -0300

    Default project created

---
+++ src/main/java/kiofarias/springframework/m2springboot/M2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package kiofarias.springframework.m2springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kiofarias/springframework/m2springboot/M2SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kiofarias.springframework.m2springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
