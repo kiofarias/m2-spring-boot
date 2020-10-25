e0db58d24ecc971a136cb07b94467dc5b67fa401
Francisco farias <kiofarias@gmail.com>
2020-10-25 10:31:31 -0300

    Finished and verified fat jar in target folder

---
+++ src/main/java/kiofarias/springframework/m2springboot/controllers/ExampleController.java
@@ -0,0 +1,20 @@
+package kiofarias.springframework.m2springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ExampleController {
+
+    @RequestMapping("/")
+    public Map<String,Object> getIndex(){
+        Map<String,Object> returnVal = new HashMap<>();
+
+        returnVal.put("Hello","World");
+
+        return returnVal;
+    }
+}
